6ecc28110db0fce81dc76ccb3d2251feaccc0c97
Brendow Lincoln <linconlbrendow@gmail.com>
2023-11-12 23:23:26 -0300

    main - Change pass value way to SelectSet widget.

--- lib/app/components/select_set/select_set_widget.dart
+++ lib/app/components/select_set/select_set_widget.dart
@@ -1,60 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:volley_scoreboard/app/components/select_set/select_set_controller.dart';
-import 'package:volley_scoreboard/app/shared/colors.dart';
-import 'package:volley_scoreboard/app/shared/enums/sets.dart';
+import 'package:volley_scoreboard/app/shared/constants/colors.dart';
+import 'package:volley_scoreboard/app/shared/constants/strings.dart';
+import 'package:volley_scoreboard/app/shared/enums/quantity_sets.dart';
 
 class SelectSetWidget extends StatefulWidget {
-  final Function? onSelectChange;
+  final QuantitySets value;
+  final Function(QuantitySets)? onSelectChange;
 
-  const SelectSetWidget({super.key, this.onSelectChange});
+  const SelectSetWidget({super.key, required this.value, this.onSelectChange});
 
   @override
   State<SelectSetWidget> createState() => _SelectSetWidgetState();
 }
 
 class _SelectSetWidgetState extends State<SelectSetWidget> {
-  final SelectSetController widgetController = SelectSetController();
-
   @override
   Widget build(BuildContext context) {
-    final SelectSetController controller = SelectSetController();
-
     return LayoutBuilder(builder: (context, constraints) {
       return Container(
         height: constraints.maxHeight,
         width: constraints.maxWidth * 0.3,
         padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child: ValueListenableBuilder(
-          valueListenable: controller,
-          builder: (context, value, child) {
-            return SegmentedButton(
-              style: ButtonStyle(
-                  textStyle: const MaterialStatePropertyAll(
-                      TextStyle(fontSize: 18.0, fontFamily: 'Anton')),
-                  backgroundColor: MaterialStateColor.resolveWith(
-                      (Set<MaterialState> states) {
-                    return states.contains(MaterialState.selected)
-                        ? AppColors.colorAccent
-                        : Colors.transparent;
-                  }),
-                  enableFeedback: false,
-                  shape: const MaterialStatePropertyAll(RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(5.0)))),
-                  foregroundColor:
-                      const MaterialStatePropertyAll(Colors.white)),
-              showSelectedIcon: false,
-              segments: const <ButtonSegment<Sets>>[
-                ButtonSegment<Sets>(value: Sets.one, label: Text('1 SET')),
-                ButtonSegment<Sets>(value: Sets.three, label: Text('3 SETS')),
-                ButtonSegment<Sets>(value: Sets.five, label: Text('5 SETS'))
-              ],
-              selected: <Sets>{value},
-              onSelectionChanged: (Set<Sets> newSelection) {
-                controller.selectSet(newSelection.first);
-              },
-            );
-          },
-        ),
+        child: SegmentedButton(
+            style: ButtonStyle(
+                textStyle: const MaterialStatePropertyAll(
+                    TextStyle(fontSize: 18.0, fontFamily: 'Anton')),
+                backgroundColor:
+                    MaterialStateColor.resolveWith((Set<MaterialState> states) {
+                  return states.contains(MaterialState.selected)
+                      ? AppColors.colorAccent
+                      : Colors.transparent;
+                }),
+                enableFeedback: false,
+                shape: const MaterialStatePropertyAll(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(5.0)))),
+                foregroundColor: const MaterialStatePropertyAll(Colors.white)),
+            showSelectedIcon: false,
+            segments: const <ButtonSegment<QuantitySets>>[
+              ButtonSegment<QuantitySets>(
+                  value: QuantitySets.one, label: Text(Strings.oneSet)),
+              ButtonSegment<QuantitySets>(
+                  value: QuantitySets.three, label: Text(Strings.threeSet)),
+              ButtonSegment<QuantitySets>(
+                  value: QuantitySets.five, label: Text(Strings.fiveSet))
+            ],
+            selected: <QuantitySets>{widget.value},
+            onSelectionChanged: (value) {
+              widget.onSelectChange!(value.first);
+            }),
       );
     });
   }
--- lib/app/pages/home_page.dart
+++ lib/app/pages/home_page.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:volley_scoreboard/app/components/select_set/select_set_widget.dart';
 import 'package:volley_scoreboard/app/controllers/home_page_controller.dart';
-import 'package:volley_scoreboard/app/shared/colors.dart';
+import 'package:volley_scoreboard/app/shared/constants/colors.dart';
+import 'package:volley_scoreboard/app/shared/enums/quantity_sets.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -33,25 +34,86 @@ class _HomePageState extends State<HomePage> {
 
     final homeController = HomePageController();
 
+    List<Widget> generateLayout(QuantitySets quantitySets) {
+      int quantityToRender = 1;
+      List<Widget> widgets = [];
+
+      switch (quantitySets) {
+        case QuantitySets.one:
+          quantityToRender = 1;
+          break;
+        case QuantitySets.three:
+          quantityToRender = 3;
+          break;
+        case QuantitySets.five:
+          quantityToRender = 5;
+          break;
+        default:
+          quantityToRender = 1;
+          break;
+      }
+
+      for (var i = 0; i < quantityToRender; i++) {
+        widgets.add(
+          Container(
+              height: screenHeight * 0.13,
+              width: screenHeight * 0.13,
+              decoration: BoxDecoration(
+                border: Border.all(color: AppColors.colorAccent, width: 1),
+              )),
+        );
+      }
+
+      return widgets;
+    }
+
     return Material(
-      child: FractionallySizedBox(
-        widthFactor: 1,
-        heightFactor: 1,
-        child: Container(
-          color: AppColors.background,
-          child: Column(
-            children: [
-              SizedBox(
-                height: screenHeight * 0.15,
-                child: SelectSetWidget(),
+      child: Container(
+        color: AppColors.background,
+        child: Column(
+          children: [
+            SizedBox(
+              height: screenHeight * 0.15,
+              child: ValueListenableBuilder(
+                  valueListenable: homeController.$selectedSetQuantityNotifier,
+                  builder: (context, value, __) {
+                    return SelectSetWidget(
+                        value: value,
+                        onSelectChange: (event) =>
+                            homeController.changeSetQuantity(event));
+                  }),
+            ),
+            Container(
+              width: screenWidth,
+              height: screenHeight * 0.85,
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.orange, width: 1),
+                  borderRadius: BorderRadius.circular(5.0)),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SingleChildScrollView(
+                    child: ValueListenableBuilder(
+                      valueListenable:
+                          homeController.$selectedSetQuantityNotifier,
+                      builder: (context, value, child) => Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: AppColors.colorAccent, width: 1),
+                            borderRadius: BorderRadius.circular(5.0)),
+                        width: screenWidth * 0.06,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: generateLayout(value),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
               ),
-              Container(
-                width: screenWidth,
-                height: screenHeight * 0.85,
-                color: Colors.brown,
-              )
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
